9fa94c190063503e66f0e4e6d1af8c659d672e4d
YuraS32 <kre.shaggy@gmail.com>
2024-06-12 20:40:47 +0200

    full tests

--- stack.py
+++ stack.py
@@ -1,22 +1,26 @@
 class Stack:
-    def __init__(self, _type = None):
-        self._items = []
+    def __init__(self, _type=None, _items=None):
         self._type = _type
+        self._items = _items if _items is not None else []
+
     def push(self, k):
-       self._items.append(k)
+        self._items.append(k)
+
     def pop(self):
-        if self._items == None:
-            return None
-        l = self._items[-1]
-        del self._items[-1]
-        return l
-        #else:
-         #   return None
+        if not self._items:
+            raise IndexError("pop from empty stack")
+        return self._items.pop()
+
     def length(self):
         return len(self._items)
+
     def get(self):
+        if not self._items:
+            raise IndexError("get from empty stack")
         return self._items[-1]
+
     def __str__(self):
-        pass
+        return str(self._items)
+
     def __repr__(self):
-        pass
+        return f"Stack(_type={repr(self._type)}, _items={self._items})"
